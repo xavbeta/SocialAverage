59677891adb70c7667689efe9b1854380940575c
Saverio Delpriori <saverio.delpriori@uniurb.it>
2016-02-15 18:21:46 +0100

    Added some stub templates

---
+++ templates/errors/invalid_token.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SAVERI0
+ * Date: 15/02/2016
+ * Time: 17:10
+ */
---
+++ templates/redeem.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SAVERI0
+ * Date: 10/02/2016
+ * Time: 11:32
+ */
+
+use SocialAverage\Templates\SocialLoginTemplate;
+
+SocialLoginTemplate::getInitTemplate();
+
+//TODO: current value
+//TODO: history
+
+//TODO: current token and share OR generate token button
+
+//TODO: redeem code button
